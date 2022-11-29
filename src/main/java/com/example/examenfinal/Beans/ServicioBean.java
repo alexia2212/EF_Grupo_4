e569f6dcb6bc7f914f5426155d26c7efed82b608
alexia2212 <a20192270@pucp.edu.pe>
2022-11-29 12:05:11 -0500

    beans usuario y servicio

---
+++ src/main/java/com/example/examenfinal/Beans/ServicioBean.java
@@ -0,0 +1,11 @@
+package com.example.examenfinal.Beans;
+
+import java.sql.Timestamp;
+
+public class ServicioBean {
+    private String tipo;
+    private Timestamp tiempoRegistro;
+    private String fechaRegistro;
+    private double costo;
+
+}
--- src/main/java/com/example/examenfinal/Beans/UsuarioBean.java
+++ src/main/java/com/example/examenfinal/Beans/UsuarioBean.java
@@ -1,4 +1,49 @@
 package com.example.examenfinal.Beans;
 
 public class UsuarioBean {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+    private String tipo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
