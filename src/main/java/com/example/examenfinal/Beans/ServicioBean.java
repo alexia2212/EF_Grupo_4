45189001cb124481d1aefa20fcf49f13405a4cce
alexia2212 <a20192270@pucp.edu.pe>
2022-11-29 12:08:49 -0500

    servicio bean

--- src/main/java/com/example/examenfinal/Beans/ServicioBean.java
+++ src/main/java/com/example/examenfinal/Beans/ServicioBean.java
@@ -4,8 +4,30 @@ import java.sql.Timestamp;
 
 public class ServicioBean {
     private String tipo;
-    private Timestamp tiempoRegistro;
-    private String fechaRegistro;
+    private java.sql.Timestamp fechaRegistro;
     private double costo;
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Timestamp getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Timestamp fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
 }
